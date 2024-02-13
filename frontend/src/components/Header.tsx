b8ee7c3b4ba2b07c05c41528da04af2bd0e4c199
rosanno <rosannomacabuhay@gmail.com>
2024-02-13 14:14:13 +0800

    add white background on signin button

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -10,7 +10,7 @@ const Header = () => {
         <span className="flex space-x-2">
           <Link
             to="/sign-in"
-            className="flex items-center text-blue-600 px-3 font-bold hover:bg-gray-100"
+            className="flex bg-white items-center text-blue-600 px-3 font-bold hover:bg-gray-100"
           >
             Sign In
           </Link>
