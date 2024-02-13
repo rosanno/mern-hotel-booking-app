57d4acdfd7bb61a30ce7ca8bc89a2096d1e413dc
rosanno <rosannomacabuhay@gmail.com>
2024-02-13 13:57:52 +0800

    add header component

--- frontend/src/layouts/Layout.tsx
+++ frontend/src/layouts/Layout.tsx
@@ -1,5 +1,11 @@
+import Header from "../components/Header";
+
 const Layout = () => {
-  return <div className="flex flex-col min-h-screen"></div>;
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+    </div>
+  );
 };
 
 export default Layout;
