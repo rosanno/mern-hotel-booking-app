26f5acf9f4073683090f53c10c3f3ad2ba1f64bf
rosanno <rosannomacabuhay@gmail.com>
2024-02-27 12:57:08 +0800

    add cookie-parser

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
 
 import userRoutes from "./routes/users";
 import authRoutes from "./routes/auth";
@@ -9,6 +10,7 @@ import authRoutes from "./routes/auth";
 mongoose.connect(process.env.MONGODB_URI as string);
 
 const app = express();
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
