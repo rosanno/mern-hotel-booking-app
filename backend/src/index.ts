e424a2c9057fb2755bcb199b6313d13d70266873
rosanno <rosannomacabuhay@gmail.com>
2024-02-27 12:44:55 +0800

    update cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,7 +11,12 @@ mongoose.connect(process.env.MONGODB_URI as string);
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
